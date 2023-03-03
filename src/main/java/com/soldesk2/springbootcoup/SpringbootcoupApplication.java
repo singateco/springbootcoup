f4f2dc8d4aad2573abf9761973d9ce05f65ab5a7
singateco <singateco@gmail.com>
2023-03-03 14:06:33 +0900

    Init by Spring Boot initilizer

---
+++ src/main/java/com/soldesk2/springbootcoup/SpringbootcoupApplication.java
@@ -0,0 +1,13 @@
+package com.soldesk2.springbootcoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcoupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcoupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soldesk2/springbootcoup/SpringbootcoupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soldesk2.springbootcoup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcoupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
