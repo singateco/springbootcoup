6208cb6d0b309d6c78c5bef21a23fa6768cbc65d
singateco <singateco@gmail.com>
2023-03-06 10:17:57 +0900

    Chat Setup

--- src/main/java/com/soldesk2/springbootcoup/websocket/controller/ChatController.java
+++ src/main/java/com/soldesk2/springbootcoup/websocket/controller/ChatController.java
@@ -1,26 +1,16 @@
 package com.soldesk2.springbootcoup.websocket.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import com.soldesk2.springbootcoup.websocket.model.Message;
 
 public class ChatController {
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
     
     @MessageMapping("/message")
-    @SendTo("/chatroom/public")
+    @SendTo("/chatroom")
     public Message receiveMessage(@Payload Message message){
-        return message;
-    }
-
-    @MessageMapping("/private-message")
-    public Message recMessage(@Payload Message message){
-        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
         return message;
     }
 }
