13f612a4616b29e45cef517d7f971f80c910ae8d
singateco <singateco@gmail.com>
2023-03-06 13:11:13 +0900

    글 mapping board로 통일

--- src/main/java/com/soldesk2/springbootcoup/controller/BoardController.java
+++ src/main/java/com/soldesk2/springbootcoup/controller/BoardController.java
@@ -16,39 +16,38 @@ import com.soldesk2.springbootcoup.entity.Board;
 import com.soldesk2.springbootcoup.service.BoardService;
 
 @RestController
-@RequestMapping("/board")
 public class BoardController {
 
 	@Autowired
 	BoardService boardService;
 	
-	@RequestMapping(value = "/Boards", method = RequestMethod.GET)
+	@RequestMapping(value = "/board", method = RequestMethod.GET)
 	public ResponseEntity<String> Find_Boards_All() { // GET / 게시글 전체 검색
 		List<Board> BoardList = boardService.get_List();
 		String jsonString = new Gson().toJson(BoardList); 
 		return ResponseEntity.status(HttpStatus.OK).body(jsonString);
 	}
 	
-	@RequestMapping(value = "/Board/{idx}", method = RequestMethod.GET)
+	@RequestMapping(value = "/board/{idx}", method = RequestMethod.GET)
 	public ResponseEntity<String> Find_Board(@PathVariable("idx") Long idx) { // GET / idx로 게시글 검색
 		Board board = boardService.get_Board(idx);
 		String jsonString = new Gson().toJson(board);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(jsonString);
 	}
 	
-	@RequestMapping(value = "/Boards", method = RequestMethod.POST)
+	@RequestMapping(value = "/board", method = RequestMethod.POST)
 	public ResponseEntity<Board> Add_Boards(@RequestBody Board board) { // POST / 게시글 추가
 		Board add_Board = boardService.add_Board(board);
 		return ResponseEntity.status(HttpStatus.CREATED).body(add_Board);
 	}
 	
-	@RequestMapping(value = "/Board", method = RequestMethod.PUT)
+	@RequestMapping(value = "/board", method = RequestMethod.PUT)
 	public ResponseEntity<Board> Update_Board(@RequestBody Board board) { // PUT / 게시글 수정
 		Board update_Borad = boardService.update_Board(board);
 		return ResponseEntity.status(HttpStatus.OK).body(update_Borad);
 	}
 	
-	@RequestMapping(value = "/Board/{idx}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/board/{idx}", method = RequestMethod.DELETE)
 	public ResponseEntity<Board> Delete_Board(@PathVariable("idx") Long idx) { // DELETE / 게시글 삭제
 		boardService.delete_Board(idx);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
